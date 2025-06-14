29ea91564fea7739d6f91bb976ebfa0e3ddb36fd
Sadam-jay <sadamjay4@gmail.com>
2025-06-14 19:01:06 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ export default {
   root: "src/",
   publicDir: "../static/",
   base: "./",
+  build: {
+    outDir: "../dist", // ← this is the key change
+    emptyOutDir: true, // optional: cleans dist before build
+  },
 };
